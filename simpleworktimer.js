76e2e9b0cedc55f88b98954c19aed32b88f820e4
Weston C. Beecroft <westoncb@gmail.com>
2017-06-29 12:06:48 -0700

    Make everything work and look cool

---
+++ simpleworktimer.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+  var lastRender = 0
+  var msPassed = 0;
+  var secondsRemaining = 0;
+
+  var finished = true;
+  var paused = false;
+
+  var keypressListener = $('.keypress-listener');
+  keypressListener.focus();
+  keypressListener.keypress(function() {
+    paused = !paused;
+
+    if (paused) {
+      $('.pause').css('display', 'block');
+    } else {
+      $('.pause').css('display', 'none');
+    }
+  });
+  
+  init();
+
+  function init() {
+    $('.hour-btn').click(function() {
+      secondsRemaining += 1 * 60 * 60;
+      finished = false;
+      keypressListener.focus();
+    });
+
+    $('.half-hour-btn').click(function() {
+      secondsRemaining += 0.5 * 60 * 60;
+      finished = false;
+      keypressListener.focus();
+    })
+
+    window.requestAnimationFrame(loop)
+  }
+
+  function update(progress) {
+
+    if (!finished && !paused) {
+      secondsRemaining -= progress / 1000;
+    }
+
+    if (secondsRemaining < 0 && !finished) {
+      secondsRemaining = 0;
+      finished = true;
+    }
+;
+    var hours = Math.floor(secondsRemaining / 60 / 60);
+    var minutes = Math.floor(secondsRemaining / 60) % 60;
+    var seconds = Math.floor(secondsRemaining) % 60;
+
+    $('.clock-text').text(hours + " : " + minutes + " : " + seconds);
+  }
+
+  function loop(timestamp) {
+    var progress = timestamp - lastRender
+
+    update(progress)
+
+    lastRender = timestamp
+    window.requestAnimationFrame(loop)
+  }
+});
